108148cc0b1496696adcb2eb13e40bc4fa548b46
Santaniel31 <alesha.napalkov@mail.ru>
2024-09-09 18:06:01 +0300

    Update daily.js

--- daily.js
+++ daily.js
@@ -11,22 +11,22 @@ const countAward = document.getElementById("countAward");
 const nextAward = document.getElementById("nextAward");
 
 button.onclick = function() {
-    dailyPoint = Math.floor(Math.random() * 71) + 30;
+    let dailyPoint = Math.floor(Math.random() * 71) + 30;
     countAward.textContent = `Вы получили: ${dailyPoint}💎`;
     nextAward.textContent = "Возвращайтесь через 12 часов";
     modal.classList.add("show");
     tg.MainButton.show()
     counter += 1;
-    
-    let data = {
-        award: dailyPoint.toString() 
-    };
 
     if (counter > 1) {
         return;
     }
-    Telegram.WebApp.onEvent("mainButtonClicked", function(){
-        tg.sendData(JSON.stringify(data));
-    });
 };
 
+let data = {
+    award: dailyPoint.toString() 
+};
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+    tg.sendData(JSON.stringify(data));
+});
