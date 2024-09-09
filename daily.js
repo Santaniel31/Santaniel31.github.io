3e1f3bfda63f319889f68f5cfefdddb190c9797b
Santaniel31 <alesha.napalkov@mail.ru>
2024-09-09 18:44:09 +0300

    Update daily.js

--- daily.js
+++ daily.js
@@ -1,24 +1,22 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
 tg.headerColor = "#17212b";
-tg.MainButton.textColor = "#FFFFFF"
+tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#008000";
 tg.MainButton.setText("Получить");
 
-let dailyPoint = Math.floor(Math.random() * 71) + 30;;
-
 let counter = 0;
-
-let data = {
-    award: dailyPoint.toString()
-};
-
 const button = document.getElementById("open-box");
 const modal = document.getElementsByClassName("modal-award")[0];
 const countAward = document.getElementById("countAward");
 const nextAward = document.getElementById("nextAward");
 
+let data = {
+    award: 0 
+};
+
 button.onclick = function() {
+    let dailyPoint = Math.floor(Math.random() * 71) + 30;
     countAward.textContent = `Вы получили: ${dailyPoint}💎`;
     nextAward.textContent = "Возвращайтесь через 12 часов";
     modal.classList.add("show");
@@ -28,14 +26,14 @@ button.onclick = function() {
     if (counter > 1) {
         tg.MainButton.setText("Закрыть");
         tg.MainButton.color = "#ff0000";
-        countAward.textContent = `Награда уже получена`;
+        countAward.textContent = "Награда уже получена";
     }
+
     data.award = dailyPoint.toString();
 };
 
-
 Telegram.WebApp.onEvent("mainButtonClicked", function() {
-    if (mainButton.text == "Закрыть") {
+    if (tg.MainButton.text === "Закрыть") {
         tg.close();
     } else {
         tg.sendData(JSON.stringify(data));
